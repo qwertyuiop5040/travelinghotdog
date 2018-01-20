37962581a4e0a5bfce58d4786e944d19c86ede19
Willievuong <willievuong@gmail.com>
2018-01-20 15:36:02 -0800

    Email/Password Authentication System

--- js/firebaseapp.js
+++ js/firebaseapp.js
@@ -9,4 +9,42 @@
     messagingSenderId: "1071086047019"
   };
   firebase.initializeApp(config);
+
+  const txtEmail= document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogOut = document.getElementById('btnLogOut');
+
+  //Log in event
+  btnLogin.addEventListen('click', e => {
+  	const email = txtEmail.value;
+  	const pass = txtPassword.value;
+  	const auth = firebase.auth();
+
+  	const promise = auth.signInWithEmailAndPassword(email,pass);
+  	promise.catch(e => console.log(e.message));
+  });
+
+  btnSignUp.addEventListen('click', e => {
+  	const email = txtEmail.value;
+  	const pass = txtPassword.value;
+  	const auth = firebase.auth();
+
+  	const promise = auth.createUserWithEmailAndPassword(email,pass);
+  	promise.catch(e => console.log(e.message));
+  });
+
+  btnLogOut.addEventListen('click', e => {
+ 	firebase.auth().signOut(); 	
+ });
+
+  firebase.auth().onAuthStateChanged(firebaseUser =>{
+  	if(firebaseUser) {
+  		console.log(firebaseUser);
+  	}else{
+  		console.log('not logged in')
+  	}
+  })
+
 }());
