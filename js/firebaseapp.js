898092e951266263a6a6ce7a7b52dfce01bac278
Willievuong <willievuong@gmail.com>
2018-01-20 13:23:06 -0800

    something I forgot

---
+++ js/firebaseapp.js
@@ -0,0 +1,12 @@
+(function(){
+	  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1hpktTGQ-26oLIo-5ien5do76eMEYspk",
+    authDomain: "travelinghotdog.firebaseapp.com",
+    databaseURL: "https://travelinghotdog.firebaseio.com",
+    projectId: "travelinghotdog",
+    storageBucket: "travelinghotdog.appspot.com",
+    messagingSenderId: "1071086047019"
+  };
+  firebase.initializeApp(config);
+}());
