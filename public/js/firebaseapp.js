d9cf8902b7712f22dc3a729f379ac6899502b363
Willievuong <willievuong@gmail.com>
2018-01-20 23:49:36 -0800

    Firebase shit

--- public/js/firebaseapp.js
+++ public/js/firebaseapp.js
@@ -1,4 +1,6 @@
-	  // Initialize Firebase
+(function(){
+
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD1hpktTGQ-26oLIo-5ien5do76eMEYspk",
     authDomain: "travelinghotdog.firebaseapp.com",
@@ -7,8 +9,8 @@
     storageBucket: "travelinghotdog.appspot.com",
     messagingSenderId: "1071086047019"
   };
+  
   firebase.initializeApp(config);
-
   var database = firebase.database();
 
   const txtEmail= document.getElementById('txtEmail');
@@ -16,21 +18,20 @@
   const btnLogIn = document.getElementById('btnLogIn');
   const btnSignUp = document.getElementById('btnSignUp');
   const btnLogOut = document.getElementById('btnLogOut');
-
+  const btnDisplay = document.getElementById('btnDisplay');
   //Log in event
   btnLogIn.addEventListener('click', e => {
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
-    talk();
-
+ 
     const promise = auth.signInWithEmailAndPassword(email,pass);
     promise.catch(e => console.log(e.message));
   });
 
   //Sign Up Event
   btnSignUp.addEventListener('click', e => {
-  	const email = txtEmail.value;
+    const email = txtEmail.value;
   	const pass = txtPassword.value;
   	const auth = firebase.auth();
 
@@ -48,13 +49,27 @@
   		console.log(firebaseUser);
   	}else{
   		console.log('not logged in')
-      talk();
   	}
   })
 
-  function talk() {
-  	firebase.database().ref('nothing').set({
-    "nothing": "there"
- 	 });
-	}
+  btnDisplay.addEventListener('click', e=>{
+    console.log(firebase.auth().currentUser.uid);
+    var userID = firebase.auth().currentUser.uid;
+    const testDisplay = document.getElementById('testvalue');
+    var refObject = firebase.database().ref('user/' + userID + 'Name');;
 
+   refObject.on('value', snap => {
+    testvalue.innerText = snap.val();
+   })
+
+   var reference = database.ref('user');
+   reference.on('value', getData);
+
+   function getData(data){
+      console.log(data.val());
+   }
+
+    
+  })
+
+}());
