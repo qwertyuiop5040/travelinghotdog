558892db8338ecb849f5278d5c5877abeaf83a32
Henry <henry08wang@gmail.com>
2018-01-20 16:18:26 -0800

    added basic skeeletonfdsdfagf

---
+++ js/sort.js
@@ -0,0 +1,9 @@
+function sortBy(num){
+	if(num=="recent"){
+		alert("Sorting By Recent Events");
+	}else if(num=="popular"){
+		alert("Sorting By Popular Events");
+	}else if(num=="recommended"){
+		alert("Sorting by reocmmendfnfdm");
+	}
+}
